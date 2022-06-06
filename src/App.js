f71eda447df1a913e859fa2a0b252723d411462b
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-06-06 15:20:07 -0400

    Added a Dark mode and Light mode to this crud api

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ import { BrowserRouter as Router, Route, Routes}
 import Home from './Home';
 import Create from './Create';
 import Edit from './Edit';
+import useLocalStorage from 'use-local-storage';
 
 function App() {
+
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  function switchTheme(){
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
+      <label className='Switch'>
+        <input type="checkbox"
+        onClick={() => switchTheme()}/>
+        <span className='slider'></span>
+      </label>
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
--- src/Database.js
+++ src/Database.js
@@ -4,6 +4,18 @@ const dataBase = [
         username: 'chickeneater1',
         email: 'chicken@gmail.com',
         password: 'chickenisgood'
+    },
+    {
+        id: '2',
+        username: 'jordan343',
+        email: 'jordan@gmail.com',
+        password: 'jordaniscool3322!'
+    },
+    {
+        id: '3',
+        username: 'd343',
+        email: 'd@gmail.com',
+        password: 'd434342!'
     }
 ];
 
